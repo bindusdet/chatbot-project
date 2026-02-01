06f355ac4b46a40d53a0ed8430a1779aa4373e24
bindusdet <bindu.sdet65@gmail.com>
2026-02-01 18:18:29 +0530

    test

--- eks/output.tf
+++ eks/output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.itkannadigaru.id
+  value = aws_eks_cluster.bindu.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.itkannadigaru.id
+  value = aws_eks_node_group.bindu.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.itkannadigaru_vpc.id
+  value = aws_vpc.bindu_vpc.id
 }
 
 output "subnet_id" {
-  value = aws_subnet.itkannadigaru_subnet[*].id
+  value = aws_subnet.bindu_subnet[*].id
 }
