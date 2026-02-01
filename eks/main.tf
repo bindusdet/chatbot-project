933184649cda8beb093b7473241f6ab4fc7ece81
bindusdet <bindu.sdet65@gmail.com>
2026-02-01 20:37:34 +0530

    location

--- eks/main.tf
+++ eks/main.tf
@@ -24,7 +24,7 @@ resource "aws_subnet" "bindu_subnet" {
   vpc_id = aws_vpc.bindu_vpc.id
   cidr_block = cidrsubnet(aws_vpc.bindu_vpc.cidr_block, 8, count.index)
 
-  availability_zone = element(["us-west-2a", "us-west-2b"], count.index)
+  availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
